5fac6f5ed69a8dff2d2069153f2ba41795cdd96b
Oleg Kozhushnyan <oleg.kozhushnyan@gmail.com>
2013-10-05 03:35:00 -0400

    Added inventory tables

--- world/database.sql
+++ world/database.sql
@@ -16,3 +16,19 @@ CREATE TABLE IF NOT EXISTS users (
        PRIMARY KEY (id),
        UNIQUE KEY (user_name)
 );
+
+CREATE TABLE IF NOT EXISTS items (
+       id INT NOT NULL AUTO_INCREMENT,
+       item_name CHAR(64) NOT NULL,
+       PRIMARY KEY (id),
+       UNIQUE KEY (item_name)
+);
+
+CREATE TABLE IF NOT EXISTS inventory (
+       user_id INT NOT NULL,
+       item_id INT NOT NULL,
+       FOREIGN KEY (user_id) REFERENCES users(id)
+       	       ON DELETE CASCADE,
+       FOREIGN KEY (item_id) REFERENCES items(id)
+       	       ON DELETE CASCADE
+);
